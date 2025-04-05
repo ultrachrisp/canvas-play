23b2341af9c9be7b675f020510014ba2d7f828b9
Chris Pyke <info@chrispyke.com>
2025-04-05 12:59:24 +0200

    build: adding some more compiler options

--- spinning-svgs/src/lib/AnimationTimer.ts
+++ spinning-svgs/src/lib/AnimationTimer.ts
@@ -48,5 +48,5 @@ export function AnimationTimer() {
 }
 
 function hasTouchSupport() {
-  return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  return "ontouchstart" in window || navigator.maxTouchPoints > 0;
 }
